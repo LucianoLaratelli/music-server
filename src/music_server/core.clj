dabe78bda93ff639f8590a49dfd548bb2523a17e
Luciano Laratelli <luciano@laratel.li>
2022-03-08 08:20:04 -0500

    add server, calls to spotify API, data parsing

---
+++ src/music_server/core.clj
@@ -0,0 +1,53 @@
+(ns music-server.core
+  "Entry point and server routes."
+  (:require
+   [jsonista.core :as j]
+   [music-server.spotify :refer [get-artists-matching-name get-album-data]]
+   [music-server.token :refer [start-token-loop]]
+   [muuntaja.core :as m]
+   [reitit.ring :as ring]
+   [reitit.ring.coercion :as coercion]
+   [reitit.ring.middleware.muuntaja :as muuntaja]
+   [ring.adapter.jetty :as jetty]
+   [ring.middleware.params :as params]))
+
+;;TODO: what happens if we get a `next` key returned in any API call?
+
+(def mapper
+  (j/object-mapper
+   {:decode-key-fn keyword}))
+
+(def app
+  (ring/ring-handler
+   (ring/router
+    [["/search-artists"
+      {:get (fn [req]
+              (let [name (get-in req [:query-params "artist_name"])]
+                (if name
+                  {:status 200
+                   :body {:artists (get-artists-matching-name name)}}
+                  {:status 404
+                   :body "got empty name"})))}]
+     ["/get-albums"
+      {:get (fn [req]
+              (let [artist (j/read-value (get-in req [:query-params "artist"]) mapper)]
+                (if artist
+                  {:status 200
+                   :body {:artists (get-album-data artist)}}
+                  {:status 404
+                   :body "got no data"})))}]]
+    {:data {:muuntaja m/instance
+            :middleware [params/wrap-params
+                         muuntaja/format-middleware
+                         coercion/coerce-exceptions-middleware
+                         coercion/coerce-request-middleware
+                         coercion/coerce-response-middleware]}})
+
+   (ring/create-default-handler)))
+
+(defonce server (do (jetty/run-jetty #'app {:port 3000, :join? false})
+                    (start-token-loop)
+                    (println "server running on 3000 at" (str (new java.util.Date)))))
+
+(defn stop []
+  (.stop server))
---
+++ src/music_server/spotify.clj
@@ -0,0 +1,103 @@
+(ns music-server.spotify
+  "Deals with calls to spotify and exposes wrappers for the server."
+  (:require
+   [clj-spotify.core :as spotify]
+   [clojure.string :as s]
+   [music-server.token :refer [token]]))
+
+(defn process-album-tracks
+  [album]
+  (->> (get-in album [:tracks :items])
+       (map #(select-keys % [:name :duration_ms :explicit]))
+       (map #(assoc % :duration_s (/ (:duration_ms %) 1000.)))
+       (map #(dissoc % :duration_ms))
+       (assoc album :tracks)))
+
+(defn sort-albums-by-date
+  [albums]
+  (into (sorted-map-by (fn [key1 key2]
+                         (let [date_0 [(get-in albums [key1 :release_date]) key1]
+                               date_1 [(get-in albums [key2 :release_date]) key2]]
+                           (compare date_0 date_1))))
+        albums))
+
+(defn pick-albums-from-duplicates
+  "Multiple albums can come in for the same title. Prefer ones with explicit tracks. If no explicit tracks are on an album, choose the first one."
+  [albums]
+  (for [album albums
+        :let [album-list (second album)
+              first-album (first album-list)]]
+    (if (= (count album-list) 1)
+      first-album
+      (let [albums-with-explicit-tracks (filter #(some :explicit (:tracks %)) album-list)]
+        (if (seq albums-with-explicit-tracks)
+          (first albums-with-explicit-tracks)
+          first-album)))))
+
+(defn albums-for-ids
+  [album-ids]
+  (when (seq album-ids)
+    (into (:albums
+           (spotify/get-several-albums
+            {:ids (s/join "," (take 20 album-ids))
+             :market "US"}
+            @token))
+          (albums-for-ids (drop 20 album-ids)))))
+
+(defn trim-deluxe-albums
+  [albums]
+  (filter #(not (s/ends-with? (:name %) "(Deluxe)")) albums))
+
+(defn album-ids-for-artist
+  [id]
+  (let [metadata (:items (spotify/get-an-artists-albums
+                          {:id id
+                           :include_groups "album"
+                           :market "US"
+                           :limit 50}
+                          @token))]
+    (->> metadata
+         trim-deluxe-albums
+         (map :id))))
+
+(defn process-artists
+  [album]
+  (assoc album :artists (map :name (:artists album))))
+
+(defn get-albums-for-artist-id
+  "Given an artist id (an alphanumeric string) return a map of all the artist's albums on spotify."
+  [id]
+  (->> id
+       album-ids-for-artist
+       albums-for-ids
+       (group-by :name)
+       pick-albums-from-duplicates
+       (map #(select-keys % [:name
+                             :total_tracks
+                             :date
+                             :release_date
+                             :artists
+                             :images
+                             :tracks]))
+       (map process-album-tracks)
+       (map process-artists)
+       (map #(assoc (dissoc % :images) :image (:url (last (:images %)))))
+       (map #(assoc (dissoc % :tracks) :tracks (map (fn [track] (select-keys track [:name :duration_s])) (:tracks %))))
+       (sort-by :release_date)))
+
+(defn get-album-data
+  [{id :id :as data}]
+  (assoc data :albums (get-albums-for-artist-id id)))
+
+(defn get-artists-matching-name
+  [name]
+  (->> (sort-by :popularity >
+                (map #(select-keys % [:name :id :popularity :external_urls :images])
+                     (filter #(> (:popularity %) 0)
+                             (get-in
+                              (spotify/search {:q name
+                                               :type "artist"}
+                                              @token)
+                              [:artists :items]))))
+       (map #(assoc (dissoc % :images) :image (:url (last (:images %)))))
+       (map #(assoc (dissoc % :external_urls) :URL (get-in % [:external_urls :spotify])))))
